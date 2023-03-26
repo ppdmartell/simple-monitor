ad6489a1c7f426e9b27d102cb900ad0d51605091
ppdmartell <ppdmartell@gmail.com>
2023-03-25 21:05:35 -0300

    Microservice for application category created.

---
+++ application/src/test/java/simplemonitor/microservice/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package simplemonitor.microservice.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
