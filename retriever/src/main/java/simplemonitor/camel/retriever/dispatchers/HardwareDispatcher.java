aa0c0a29761bfd64b1c066dd75e90f7806de05cf
ppdmartell <ppdmartell@gmail.com>
2023-03-26 23:09:23 -0300

    Bug related to the save a new category and dispatch was fixed.

--- retriever/src/main/java/simplemonitor/camel/retriever/dispatchers/HardwareDispatcher.java
+++ retriever/src/main/java/simplemonitor/camel/retriever/dispatchers/HardwareDispatcher.java
@@ -19,7 +19,6 @@ public class HardwareDispatcher extends Dispatcher {
 	@Override
 	public void dispatch(CategoryDto category) {
 		// TODO Auto-generated method stub
-		logger.info("Dispatching - > [" + category.getCategory() + "] [" + category.getIndice() + "]");
 	}
 
 }
