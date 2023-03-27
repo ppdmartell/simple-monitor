0c94487acdd3d9ccbe4b32801d45c9a7fa719efb
ppdmartell <ppdmartell@gmail.com>
2023-03-26 23:10:44 -0300

    Added logger to know when a POST request is received from camel.

--- application/src/main/java/simplemonitor/microservice/application/controllers/ApplicationController.java
+++ application/src/main/java/simplemonitor/microservice/application/controllers/ApplicationController.java
@@ -2,6 +2,8 @@ package simplemonitor.microservice.application.controllers;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,8 +16,11 @@ import simplemonitor.microservice.application.dto.CategoryDto;
 @RequestMapping("/api/category")
 public class ApplicationController {
 	
+	Logger logger = LoggerFactory.getLogger(ApplicationController.class);
+	
 	@PostMapping("/application")
 	public ResponseEntity<String> receiveCategoryDto(@Valid @RequestBody CategoryDto categoryDto) {
+		logger.info("Received a CategoryDto object: " + categoryDto.toString());
 		return ResponseEntity.ok(categoryDto.toString());
 	}
 
