4b8013395cac4487e4d22e5dcb4a8cea7da21b59
ppdmartell <ppdmartell@gmail.com>
2023-03-28 19:46:42 -0300

    Setting things for webflux.

--- application/src/main/java/simplemonitor/microservice/application/Application.java
+++ application/src/main/java/simplemonitor/microservice/application/Application.java
@@ -2,9 +2,16 @@ package simplemonitor.microservice.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class Application {
+	
+	@Bean
+	public WebClient.Builder getBuilder() {
+		return WebClient.builder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
