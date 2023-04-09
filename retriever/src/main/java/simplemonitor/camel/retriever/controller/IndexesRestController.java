9582b143a02d953acac9be1d57d594109564b266
ppdmartell <ppdmartell@gmail.com>
2023-04-09 20:29:44 -0300

    Get the name of the existing hosts.

--- retriever/src/main/java/simplemonitor/camel/retriever/controller/IndexesRestController.java
+++ retriever/src/main/java/simplemonitor/camel/retriever/controller/IndexesRestController.java
@@ -1,11 +1,15 @@
 package simplemonitor.camel.retriever.controller;
 
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import simplemonitor.camel.retriever.model.entity.Category;
 import simplemonitor.camel.retriever.restdto.TotalIndexesDto;
 import simplemonitor.camel.retriever.service.ICategoryService;
 
@@ -24,6 +28,18 @@ public class IndexesRestController {
 		totalIndexesDto.setDescription("All the registered indices.");
 		totalIndexesDto.setIndices(categoryService.findAll());
 		return totalIndexesDto;
+	}
+	
+	@GetMapping("/hosts")
+	public List<String> getHosts() {
+		List<String> hosts = categoryService.findAll()
+				                .stream()
+								.map(Category::getIndice)
+								.map(category -> category.replaceAll("-(19|20)\\d\\d\\.(0[1-9]|1[012])\\.(0[1-9]|[12][0-9]|3[01])", ""))
+								.map(category -> category.replaceAll("(kernel|user|hardware|security|application|boot)-", ""))
+								.distinct()
+								.collect(Collectors.toList());
+		return hosts;
 	}
 
 }
